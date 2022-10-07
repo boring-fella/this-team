fff3cf29bf334488888b253c8e5fea2e4c571791
Nick Leso <nickleso.work@gmail.com>
2022-10-07 17:02:59 +0300

    Add fetch classes
    - add fetch popular films in fetch-popular-films.js;
    - add fetch films in fetch-films.js;
    - change path to index.js in index.html & library.html;
    - add test listeners and funcs;
    - creare filmoteca-popular.js

--- src/js/filmoteka.js
+++ src/js/filmoteka.js
@@ -1,2 +1,22 @@
 import FilmsAPI from './fetch/fetch-films';
 import PopFilmsAPI from './fetch/fetch-popular-films';
+
+const refs = {
+  //   formEl: document.querySelector('#search-form'),
+  testEl: document.querySelector('.section-main'),
+};
+
+// refs.formEl.addEventListener('submit', onFormSubmit);
+
+refs.testEl.addEventListener('click', onTestElClick);
+
+const popFilmsSerchAPI = new PopFilmsAPI();
+const filmsSerchAPI = new FilmsAPI();
+
+// test functions
+function onTestElClick(event) {
+  console.log('Клик по секции и приход популярных фильмов с бэка:');
+
+  popFilmsSerchAPI.fetchPopFilms().then(data => console.log(data));
+}
+// end of the test functions
