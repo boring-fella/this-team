526ea85a1520eebfdbb4c2a7a70e086109de01ac
Nick Leso <nickleso.work@gmail.com>
2022-10-08 11:23:56 +0300

    Update film-cards-markup.js

--- src/js/markup/film-cards-markup.js
+++ src/js/markup/film-cards-markup.js
@@ -23,9 +23,11 @@ function createFilmCardMarkup(film) {
           <img class="film-card__image" src="${BASE_IMAGES_URL}${poster_path}" alt="${title}"/>
           <div class="film-card__features-wrap">
             <p class="film-card__title">${title}</p>
-            <p class="film-card__features">${genre_ids} |</p>
-            <p class="film-card__date">${release_date}</p>
-            <p class="film-card__rating">${vote_average}</p>
+            <div class="film-card__tech-wrap">
+              <p class="film-card__features">${genre_ids} |</p>
+              <p class="film-card__date">${release_date}</p>
+              <p class="film-card__rating">${vote_average}</p>
+            </div>
           </div>
         </a>`;
       }
