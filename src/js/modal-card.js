352f5677f6c7a66acbf5cf2b5e95fae47cf082ab
Hanna Potapova <an.al.potapova@gmail.com>
2022-10-08 14:24:44 +0300

    modal

---
+++ src/js/modal-card.js
@@ -0,0 +1,18 @@
+(() => {
+const refs = {
+    filmCard: document.querySelector('.film-container'),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+    };
+
+    refs.filmCard.addEventListener('click', modalIsHidden);
+    refs.closeModalBtn.addEventListener("click", visibilityIsHidden);
+
+function modalIsHidden() {
+  refs.modal.classList.add('is-hidden');
+}
+function visibilityIsHidden() {
+  refs.modal.classList.remove('is-hidden');
+}
+    
+})();
