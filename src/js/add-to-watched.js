80fd40edcdf99dc6de36fe9fb4a34894fcdbf462
Ruslan Babkin <ruslan.babkin01@gmail.com>
2022-10-09 16:55:55 +0300

    fix logo+import

--- src/js/add-to-watched.js
+++ src/js/add-to-watched.js
@@ -1,22 +1,20 @@
-// import { save, load, remove } from './local-storage-api';
+import storageAPI from './local-storage-api';
 
 const LOC_STOR_KEY_WATCHED = 'watched-key';
 const addBtnRef = document.querySelector('.btn__add-watched');
 
 addBtnRef.addEventListener('Click', onClickBtnAddWatched);
 
-onClickBtnAddWatched();
-
 function onClickBtnAddWatched(e) {
-  const savedData = load(LOC_STOR_KEY_WATCHED);
+  const savedData = storageAPI.load(LOC_STOR_KEY_WATCHED);
 
   if (!savedData) {
-    //  save(LOC_STOR_KEY_WATCHED);
+    //  storageAPI.save(LOC_STOR_KEY_WATCHED);
     addBtnRef.textContent = 'delete from watched';
     addBtnRef.style.backgroundColor = '#ff6b01';
   }
 
-  remove(LOC_STOR_KEY_WATCHED);
+  storageAPI.remove(LOC_STOR_KEY_WATCHED);
   addBtnRef.textContent = 'add to watched';
   addBtnRef.style.backgroundColor = 'transparent';
 }
