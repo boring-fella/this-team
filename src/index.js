5c4a9af7594e8fe09ddf17aa6e377b444bac1dee
Bohdan <makohoniuk.bo@gmail.com>
2022-10-10 20:29:23 +0300

    розкоментовує import в файлі index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import './js/fetch/fetch-genres';
 // import './js/filmoteka-popular';
 // import './js/filmoteka';
 import './js/library';
-// import './js/modal-card';
+import './js/modal-card';
 import './js/scroll/scroll-to-top';
 
 // import './js/watched';
 import './js/pagination';
 import './js/queue';
+import './js/watched';
 
 import './js/add-to-watched';
-// import './js/modal-info';
+import './js/modal-info';
