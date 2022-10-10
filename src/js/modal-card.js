e2e98668a4722ea82f99daf0096060c74e02ac7b
Andrey5353 <andreyae0117ab@gmail.com>
2022-10-10 16:08:11 +0300

    modal-card
    добавил закрытие модалки по Esc

--- src/js/modal-card.js
+++ src/js/modal-card.js
@@ -1,12 +1,33 @@
 (() => {
-const refs = {
+  const refs = {
     filmCard: document.querySelector('.film-container'),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
-    };
+  };
   
     refs.filmCard.addEventListener('click', modalIsHidden);
     refs.closeModalBtn.addEventListener("click", visibilityIsHidden);
+  
+  // закрываем модалку по Esc
+  document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      visibilityIsHidden();
+    }
+  });
+
+// должно бы закрывать по клику в стороне... но все наоборот...
+  
+  
+  // document.addEventListener('click', evt => {
+  //   const target = evt.target
+  //   if (!target.closest('.modal')) {
+  //     return
+  //   }
+  //   visibilityIsHidden();
+  // });
+
+
+
 
 function modalIsHidden() {
   refs.modal.classList.add('is-hidden');
