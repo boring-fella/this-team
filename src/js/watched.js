649eff147b5bfa4016591f94c5899a8572749c10
Bohdan <makohoniuk.bo@gmail.com>
2022-10-10 20:47:37 +0300

    Спроба-2

--- src/js/watched.js
+++ src/js/watched.js
@@ -1,13 +1,9 @@
 import storageAPI from './local-storage-api';
-<<<<<<< Updated upstream
 import { saveCurrentFilmsToLocal, getFilmFromLocal } from './display-films';
 import FilmCards from './markup/film-cards-markup';
 import { hideElement } from './markup/hide-elements';
-import viewFilmQueue from './queue'
+import { viewFilmQueue } from './queue'
 
-=======
-import {viewFilmQueue} from './queue'
->>>>>>> Stashed changes
 
 
 
