e3eccc9d604490391a5b4cd7a0382464ec865e8f
Bohdan <makohoniuk.bo@gmail.com>
2022-10-10 20:09:28 +0300

    додає логіку роботи кнопки watched

--- src/js/watched.js
+++ src/js/watched.js
@@ -1,9 +1,13 @@
 import storageAPI from './local-storage-api';
+<<<<<<< Updated upstream
 import { saveCurrentFilmsToLocal, getFilmFromLocal } from './display-films';
 import FilmCards from './markup/film-cards-markup';
 import { hideElement } from './markup/hide-elements';
 import viewFilmQueue from './queue'
 
+=======
+import {viewFilmQueue} from './queue'
+>>>>>>> Stashed changes
 
 
 
