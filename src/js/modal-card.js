eac1245f1a6963ff422ed214d8849e4460d07085
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2022-10-11 22:07:42 +0200

    Update modal-card.js

--- src/js/modal-card.js
+++ src/js/modal-card.js
@@ -1,39 +1,37 @@
-(() => {
-  const refs = {
-    filmCard: document.querySelector('.film-container'),
-    filmCardLibrary: document.querySelector('.please-choose'),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+const refs = {
+  filmCard: document.querySelector('.film-container'),
+  filmCardLibrary: document.querySelector('.please-choose'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+};
 
-  try {
+try {
   refs.filmCard.addEventListener('click', modalIsHidden);
 } catch (error) {
   refs.filmCardLibrary.addEventListener('click', modalIsHidden);
 }
-  
-  refs.closeModalBtn.addEventListener("click", visibilityIsHidden);
-  
-  // закрываем модалку по Esc
-  document.addEventListener('keydown', function (evt) {
 
-    if (evt.key === 'Escape') {
-      visibilityIsHidden();
-    }
-  });
+refs.closeModalBtn.addEventListener('click', visibilityIsHidden);
 
-  // Закрывает модалку по клику в стороне
-  document.addEventListener('click', function (evt) {
-    if (evt.target === refs.modal) {
-      visibilityIsHidden();
-    }
-  });
+function closeByEsc(evt) {
+  if (evt.key === 'Escape') {
+    visibilityIsHidden();
+  }
+}
+// Закрывает модалку по клику в стороне
+document.addEventListener('click', function (evt) {
+  if (evt.target === refs.modal) {
+    visibilityIsHidden();
+  }
+});
 
 function modalIsHidden() {
   refs.modal.classList.add('is-hidden');
+  document.addEventListener('keydown', closeByEsc);
   // modal.addEventListener('click', overlayClose);
 }
+
 function visibilityIsHidden() {
   refs.modal.classList.remove('is-hidden');
+  document.removeEventListener('keydown', closeByEsc);
 }
-})();
