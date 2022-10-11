13ac07f0ff15bfcad8f474682d115c9078085575
Andrey5353 <andreyae0117ab@gmail.com>
2022-10-11 12:00:08 +0300

    modal-card
    Закрывает модалку по клику в сторону

--- src/js/modal-card.js
+++ src/js/modal-card.js
@@ -5,36 +5,28 @@
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
-  
+
   try {
   refs.filmCard.addEventListener('click', modalIsHidden);
 } catch (error) {
   refs.filmCardLibrary.addEventListener('click', modalIsHidden);
 }
   
-    // refs.filmCard.addEventListener('click', modalIsHidden);
-    refs.closeModalBtn.addEventListener("click", visibilityIsHidden);
+  refs.closeModalBtn.addEventListener("click", visibilityIsHidden);
   
   // закрываем модалку по Esc
   document.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === 27) {
+    if (evt.key === 'Escape') {
       visibilityIsHidden();
     }
   });
 
-// должно бы закрывать по клику в стороне... но все наоборот...
-  
-  
-  // document.addEventListener('click', evt => {
-  //   const target = evt.target
-  //   if (!target.closest('.modal')) {
-  //     return
-  //   }
-  //   visibilityIsHidden();
-  // });
-
-
-
+  // Закрывает модалку по клику в стороне
+  document.addEventListener('click', function (evt) {
+    if (evt.target === refs.modal) {
+      visibilityIsHidden();
+    }
+  });
 
 function modalIsHidden() {
   refs.modal.classList.add('is-hidden');
