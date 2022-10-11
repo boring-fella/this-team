71ec99db7dbd5a7b589311e236d69d26216e4554
Ruslan Babkin <ruslan.babkin01@gmail.com>
2022-10-11 12:08:22 +0300

    fix-btn-add

--- src/js/add-to-watched.js
+++ src/js/add-to-watched.js
@@ -1,42 +1,56 @@
 import storageAPI from './local-storage-api';
-import { getFilmFromLocal } from './display-films';
+import { saveCurrentFilmsToLocal, getFilmFromLocal } from './display-films';
 
-const addBtnRef = document.querySelector('.btn__add-watched');
-const filmContRef = document.querySelector('.film-container');
+const refs = {
+  addBtn: document.querySelector('.btn__add-watched'),
+  filmContainer: document.querySelector('.film-container'),
+};
 
-addBtnRef.addEventListener('click', onClickBtnAddToWatched);
-filmContRef.addEventListener('click', getFilmFromLocal);
+refs.filmContainer.addEventListener('click', getFilmFromLocal);
+refs.addBtn.addEventListener('click', onClickBtnAddToWatch);
 
-function onClickBtnAddToWatched() {
-  const newFilm = storageAPI.load('newfilm');
+// Перевіряє чи є фільм в переглянутих
+function checkFilmInLocStor() {
+  const newFilm = storageAPI.load('currentFilm');
   let watchedFilms = storageAPI.load('watchedFilms');
   watchedFilms = watchedFilms ? watchedFilms : [];
-
   const isFilmNotInclude =
     watchedFilms.find(film => film.id === newFilm.id) === undefined;
 
-  console.log(isFilmNotInclude);
+  return isFilmNotInclude;
+}
 
+// Додає в масив переглянутих фільмів в локалсторедж
+function onClickBtnAddToWatch() {
+  const isFilmNotInclude = checkFilmInLocStor();
+  const newFilm = storageAPI.load('currentFilm');
+  let watchedFilms = storageAPI.load('watchedFilms');
+  watchedFilms = watchedFilms ? watchedFilms : [];
   if (isFilmNotInclude) {
-    watchedFilms = watchedFilms.concat(newFilm);
-    storageAPI.save('watchedFilms', newFilm);
+    watchedFilms.push(newFilm);
+    storageAPI.save('watchedFilms', watchedFilms);
 
-    addBtnDeleteWatched();
+    addBtnRemoveWatched();
   } else {
     const filmRemove = watchedFilms.findIndex(film => film.id === newFilm.id);
     watchedFilms.splice(filmRemove, 1);
     storageAPI.save('watchedFilms', watchedFilms);
+    saveCurrentFilmsToLocal(watchedFilms);
 
     addBtnToWatched();
   }
 }
 
-function addBtnDeleteWatched() {
-  addBtnRef.textContent = 'delete from watched';
-  addBtnRef.style.backgroundColor = '#ff6b01';
+function addBtnRemoveWatched() {
+  if (refs.addBtn.textContent === 'add to Watched') {
+    refs.addBtn.textContent = 'remove from watched';
+    refs.addBtn.classList.add('btn-add__active');
+  }
 }
 
 function addBtnToWatched() {
-  addBtnRef.textContent = 'add to watched';
-  addBtnRef.style.backgroundColor = '#fff';
+  if (refs.addBtn.textContent === 'remove from watched') {
+    refs.addBtn.textContent = 'add to Watched';
+    refs.addBtn.classList.add('btn-add__active');
+  }
 }
