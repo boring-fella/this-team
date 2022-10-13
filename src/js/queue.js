05997ef9141e6037b8fe1643605b1ebf009f01b5
Viacheslav Tykhovlis <front.tykhovlis@gmail.com>
2022-10-13 22:33:22 +0200

    Изменения в описании
    1. Добавляет котика, если в каком-то списке библиотек нет фильмов.
    2. Уменьшен размер картинки с котиком.
    3. Исправлена логика неотображения модалки при клике между карточками или в пустой библиотеке.

--- src/js/queue.js
+++ src/js/queue.js
@@ -5,6 +5,7 @@ import {
   changeColorRating,
 } from './display-films';
 import FilmCards from './markup/film-cards-markup';
+import { ifNotLibrary } from './markup/clear-markup';
 
 let queueEl;
 
@@ -13,15 +14,23 @@ const filmLibrary = document.querySelector('.please-choose');
 const viewQueue = document.querySelector('#queue');
 const viewWatched = document.querySelector('#watched');
 
+// вішаємо слухачів
 try {
   filmContainer.addEventListener('click', getFilmFromLocal);
+  try {
+    filmLibrary.removeEventListener('click', getFilmFromLocal);
+    viewQueue.removeEventListener('click', clickOnQueue);
+  } catch (error) {}
 } catch (error) {
   filmLibrary.addEventListener('click', getFilmFromLocal);
   viewQueue.addEventListener('click', clickOnQueue);
-
+  try {
+    filmContainer.removeEventListener('click', getFilmFromLocal);
+  } catch (error) {}
   clickOnQueue();
 }
 
+// додаємо або видаляємо фильм до/з списку фильмів в черзі при кліку на кнопці в модалці
 function changeListQueue() {
   const isFilmNotInclude = filmNotQueue();
   const actualFilm = storageAPI.load('currentFilm');
@@ -31,7 +40,6 @@ function changeListQueue() {
     queueFilms.push(actualFilm);
     storageAPI.save('queueFilms', queueFilms);
     changeButtonForRemove();
-    // console.log('добавили фильм');
   } else {
     const filmForRemove = queueFilms.findIndex(
       film => film.id === actualFilm.id
@@ -47,6 +55,7 @@ function changeListQueue() {
   }
 }
 
+// змінює колір та напис на кнопці в модалці щодо фильмів в черзі (ФИЛЬМ У ЧЕРЗІ)
 export function changeButtonForRemove() {
   if (queueEl.textContent === 'add to queue') {
     queueEl.textContent = 'remove from queue';
@@ -54,6 +63,7 @@ export function changeButtonForRemove() {
   }
 }
 
+// змінює колір та напис на кнопці в модалці щодо фильмів в черзі (фильм НЕ у черзі)
 export function changeButtonForAdd() {
   if (queueEl.textContent === 'remove from queue') {
     queueEl.textContent = 'add to queue';
@@ -61,23 +71,35 @@ export function changeButtonForAdd() {
   }
 }
 
+// відображає фильми, які є в бібліотеці
 export function viewFilmLibrary() {
   if (viewQueue.classList.contains('btn-add__active')) {
     try {
       let queueFilms = storageAPI.load('queueFilms');
+      if (queueFilms.length === 0) {
+        ifNotLibrary();
+        return;
+      }
       saveCurrentFilmsToLocal(queueFilms);
+      filmLibrary.classList.remove('please-choose__empty');
       filmLibrary.innerHTML = FilmCards.createFilmCardMarkup(queueFilms);
     } catch (error) {}
   }
   if (viewWatched.classList.contains('btn-add__active')) {
     try {
       let watchedFilms = storageAPI.load('watchedFilms');
+      if (watchedFilms.length === 0) {
+        ifNotLibrary();
+        return;
+      }
       saveCurrentFilmsToLocal(watchedFilms);
+      filmLibrary.classList.remove('please-choose__empty');
       filmLibrary.innerHTML = FilmCards.createFilmCardMarkup(watchedFilms);
     } catch (error) {}
   }
 }
 
+// перевіряє чи немає фильму в черзі. Повертає "правда", якщо фільму немає
 export function filmNotQueue() {
   const actualFilm = storageAPI.load('currentFilm');
   let queueFilms = storageAPI.load('queueFilms');
@@ -87,6 +109,7 @@ export function filmNotQueue() {
   return isFilmNotInclude;
 }
 
+// запускає запуск відображення фільмів в бібліотеці queue
 function clickOnQueue() {
   viewQueue.classList.add('btn-add__active');
   viewWatched.classList.remove('btn-add__active');
@@ -94,6 +117,7 @@ function clickOnQueue() {
   changeColorRating();
 }
 
+// шукає кнопку QUEUE після рендеру модалки
 export function findBtnQueue() {
   queueEl = document.querySelector('.btn-queue-js');
   queueEl.addEventListener('click', changeListQueue);
