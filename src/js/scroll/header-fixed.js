7dc1806e27f7c2cd86a20f3de06e85defd398703
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2022-10-13 20:09:46 +0200

    loader
    different from header dimentions loader position

--- src/js/scroll/header-fixed.js
+++ src/js/scroll/header-fixed.js
@@ -6,9 +6,12 @@ document.addEventListener('scroll', onHeaderScroll);
 
 function onHeaderScroll() {
   if (window.scrollY > headerHeight / 3) {
+    document.querySelector('.preloader').classList.add('preloader-fixed');
+
     headerContainer.classList.add('is-fixed');
     sectionMain.style.paddingTop = headerHeight + 'px';
   } else {
+    document.querySelector('.preloader').classList.remove('preloader-fixed');
     headerContainer.classList.remove('is-fixed');
     sectionMain.removeAttribute('style');
   }
