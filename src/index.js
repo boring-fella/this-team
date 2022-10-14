7a36de19aa46e7f653773343401d46a7a99dce86
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2022-10-14 07:35:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import './js/watched';
 import './js/add-to-watched';
 import './js/team-modal';
 import './js/loader';
-
 import './js/scroll/header-fixed';
-import './js/scroll/header-library';
+
+//import './js/scroll/header-library'; 
 
 
 
